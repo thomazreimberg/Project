232204a03877d86255a7fde7617d73371ed37dc3
Thomaz Henrique <treimberg7@gmail.com>
2020-06-12 22:37:45 -0300

    utils completed
    crypto, decode and user completed

--- backend/src/controller/officeController.ts
+++ backend/src/controller/officeController.ts
@@ -1,11 +1,13 @@
-import { Request, Response } from 'express';
+import { Request, Response, request } from 'express';
 import knex from '../database/connection';
-
+const decode = require('../utils/decode');
+const crypto = require('../utils/crypto');
 class PointController {
     async show(request: Request, response:Response) {
         const offices =  await knex('tb_office')
             .select('ds_office');
-
+        console.log(decode('t', 2));
+        console.log(crypto('r', 2));
         return response.json(offices);
     }
 }
